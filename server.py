f996c8838ab97897287fb37fbfab35eb6dbbc7ad
Jake Coker <jake.coker91@gmail.com>
2020-02-20 14:59:52 -0800

    Setup for layout.html

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def mainPage():
     return render_template('index.html')
+
